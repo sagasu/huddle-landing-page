eca2089479f1c7a7bfe51a6250b14ab814a147ed
Mateusz Kopij <sagasu@gmail.com>
2021-09-26 03:22:07 +0100

    introducing header component with styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import {Container} from './components/styles/Container.styled'
-
+import Header from './components/header'
 
 function App() {
   return (
+    <>
+    <Header></Header>
     <Container>
       <h1>hello</h1>
     </Container>
+    </>
   );
 }
 
