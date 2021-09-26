5442a7e39ffe2c2b7927d6359a34071e8ee89222
Mateusz Kopij <sagasu@gmail.com>
2021-09-26 01:03:02 +0100

    creating first styled component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
+import {Container} from './components/styles/Container.styled'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <h1>hello</h1>
+    </Container>
   );
 }
 
