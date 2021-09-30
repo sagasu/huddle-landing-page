b5e0d91560d2b0dd902e59e7fe578ee559ccd866
Mateusz Kopij <sagasu@gmail.com>
2021-09-30 11:00:14 +0100

    creating a global style and passing style parameters as props.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import {Container} from './components/styles/Container.styled'
-import Header from './components/header'
+import {Container} from './components/styles/Container.styled';
+import Header from './components/header';
+import {ThemeProvider} from 'styled-components';
+import GlobalStyles from './components/styles/Global';
+
+const theme = {
+  
+  colors: {
+    headers: '#ebfbff',
+    body: '#fff',
+    footer: '#003333'
+  }
+}
 
 function App() {
+  // really cool way of importing global styles
   return (
-    <>
-    <Header></Header>
-    <Container>
-      <h1>hello</h1>
-    </Container>
-    </>
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyles />
+        <Header></Header>
+        <Container>
+          <h1>hello</h1>
+        </Container>
+      </>
+    </ThemeProvider>
   );
 }
 
--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,9 @@
 import { StyledHeader } from "./styles/Header.styled"
 
 export default function Header(){
+    // I can pass parameters in StyledHeader as many as I like, I don't use it here because I play with theme on top level, but it is easily doable. 
     return (
-        <StyledHeader>
+        <StyledHeader bg="red">
             <h1>Hubble</h1>
         </StyledHeader>
         )
