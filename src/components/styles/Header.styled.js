5d2bedad4ac26826f90e9417ad97b9d733e2e9f1
Mateusz Kopij <sagasu@gmail.com>
2021-09-30 10:30:17 +0100

    experiments with extra styling

--- src/components/styles/Header.styled.js
+++ src/components/styles/Header.styled.js
@@ -3,5 +3,11 @@ import styled from "styled-components";
 export const StyledHeader = styled.header`
     background-color: #ebfbff;
     padding: 40px 0;
-
+    h1 {
+        color:red;
+    }
+    
+    &:hover {
+        background-color: black;
+    }
 `
